3e613fa02436d970307217f78b3386a480f3469d
md-zeon <zeon.cse@gmail.com>
2026-03-05 22:41:04 +0600

    My First Program in Python

---
+++ 01.first_program.py
@@ -0,0 +1,28 @@
+print("Hello Zeon!")
+# print is a function that outputs the specified message to the screen. In this case, it will display "Hello Zeon!" when the program is run.
+# The text inside the parentheses and quotes is called a string, which is a sequence of characters. In this case, the string is "Hello Zeon!".
+
+print("Hello World!", "With \nPython!")
+
+# The second print statement demonstrates that you can print multiple strings at once by separating them with a comma.
+# The "\n" is an escape character that creates a new line, so the output will be:
+# Hello World! With
+# Python!
+
+print("Hello Zeon!", end=" ")
+print("Welcome to Python programming!")
+# In the third and fourth print statements, we use the 'end' parameter to specify what should be printed at the end of the output.
+# By default, the 'end' parameter is set to a newline character ("\n"), which is why each print statement starts on a new line. However, by setting 'end' to a space (" "), we can make the second print statement continue on the same line as the first one.
+# The output of the above code will be:
+# Hello Zeon! Welcome to Python programming!
+
+
+print (10 + 5)
+# This line of code will output the result of the addition operation, which is 15. The numbers 10 and 5 are added together, and the result is printed to the screen.
+
+# Character Set
+# Letters: A - Z, a - z
+# Digits: 0 - 9
+# Special Symbols: +, - * / % etc.
+# Whitespaces: Blank Space, Tab, Newline etc.
+# all ASCII & Unicode characters as part of data & literals in Python.
