43ff4b3c891cf8ada7860f0a48146c10a8e42261
md-zeon <zeon.cse@gmail.com>
2026-03-05 23:02:23 +0600

    Learned about Keywords in python

---
+++ 04.Keywords.py
@@ -0,0 +1,14 @@
+# KeyWords: Keywords are reserved words in Python that have special meanings and cannot be used as identifiers (like variable names, function names, etc.). They are predefined by the language and serve specific purposes in the syntax and structure of Python code.
+# There are many keywords in Python
+# They are:
+#* False      None       True        and         as          assert
+#* async     await      break       class       continue    def
+#* del       elif       else        except      finally     for
+#* from      global     if          import      in          is
+#* lambda     nonlocal   not         or          pass        raise
+#* return     try        while       with        yield       case
+
+'''
+This is a multi-line comment in Python. It is often used for documentation or to provide explanations about the code. Multi-line comments are enclosed within triple quotes (\''' or \"""). They can span multiple lines and are ignored by the Python interpreter when the code is executed.
+'''
+#* This is a single-line comment in Python. It is used to provide explanations or notes about the code. Single-line comments start with the hash symbol (#) and continue until the end of the line. They are also ignored by the Python interpreter when the code is executed.
