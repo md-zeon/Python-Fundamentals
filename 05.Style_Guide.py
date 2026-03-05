4758a9ed0d106a33cb373bd05a974e00b56fd23f
md-zeon <zeon.cse@gmail.com>
2026-03-05 23:05:32 +0600

    Added style guide for variable naming conventions in Python

---
+++ 05.Style_Guide.py
@@ -0,0 +1,8 @@
+# There are 4 types of Styles
+# 1. Camel Case: In this style, the first letter of each word is capitalized, and there are no spaces or underscores between words. Example: MyVariableName, UserProfile, EmployeeData
+# 2. Pascal Case: Similar to Camel Case, but the first letter of the first
+#    word is also capitalized. Example: MyVariableName, UserProfile, EmployeeData
+# 3. Snake Case: In this style, all letters are lowercase, and words are separated by underscores. Example: my_variable_name, user_profile, employee_data
+# 4. Kebab Case: In this style, all letters are lowercase, and words are separated by hyphens. Example: my-variable-name, user-profile, employee-data
+# In Python, the most commonly used style for variable names and function names is Snake Case. For example:
+my_variable_name = "Hello, World!"
