2c91f8af1c9e5f99bddec931a7768061013b636d
md-zeon <zeon.cse@gmail.com>
2026-03-06 14:46:59 +0600

    Added solution for user name and age input assignment

--- part-1/11.Assignment.py
+++ part-1/11.Assignment.py
@@ -4,6 +4,10 @@ Q1. Write a program that asks the user for their name and age, then prints a
 sentence like: 
 Hello Shradha, you are 21 years old! 
 '''
+#* Q1 Solution:
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hello", name + ",", "you are", age, "years old!")
 
 
 '''
