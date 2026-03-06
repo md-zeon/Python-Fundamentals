dcf2416c7e93d48886b66c20ee8dd2e8bce80cac
md-zeon <zeon.cse@gmail.com>
2026-03-06 14:46:34 +0600

    Add assignment problems for user input and data conversion exercises

---
+++ part-1/11.Assignment.py
@@ -0,0 +1,60 @@
+'''
+Assignment Problems 
+Q1. Write a program that asks the user for their name and age, then prints a 
+sentence like: 
+Hello Shradha, you are 21 years old! 
+'''
+
+
+'''
+Q2. Take two numbers as input from the user and print their sum, difference, 
+product, and quotient. 
+'''
+
+'''
+Q3. Ask the user to enter two integers and one float. Convert them all to floats 
+and print their average. 
+'''
+
+'''
+Q4. The user enters a string containing a number (e.g., ). Convert it to: 
+• an integer 
+• a float 
+• a string again 
+Print all three values with their types.
+'''
+
+'''
+Q5. Evaluate and print the result of the following expression: 
+x = 10 + 3 * 2 ** 2 
+Based on what you learnt in the lecture explain why the output is what it is. 
+'''
+
+'''
+Q6. Write a program to swap values of two numbers entered by the user.
+'''
+
+'''
+Q7. Ask the user for a temperature in Celsius (string input). Convert it to float, 
+then calculate and print temperature in Fahrenheit. 
+float 
+Conversion formula: FahrenheitTemp = (CelsiusTemp * (9/5)) + 32 
+'''
+
+'''
+Q8. Take the radius (r) as user input and print the area. 
+Use the formula:  Area = π * r2   (value of π = 3.14) 
+'''
+
+'''
+Q9. Ask the user for: Principal (P), Rate (R), Time (T). Convert all to float and 
+compute simple interest: 
+SI = (P * R * T )/100 
+'''
+
+
+'''
+Q10. Take a decimal number as input (like 45.78) and output its: 
+• integer part - 45 
+• fractional part - .78
+'''
