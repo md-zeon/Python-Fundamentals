b25e656af1cd7cca5d7467bac2c448b852c51a45
md-zeon <zeon.cse@gmail.com>
2026-03-10 11:06:28 +0600

    Refactor student enrollment example to improve clarity and structure

--- part-3/08.Practice.py
+++ part-3/08.Practice.py
@@ -16,6 +16,25 @@ info = [
 
 # List all unique courses
 unique_courses = set()
+print("Unique courses:", end=" ")
 for student, course in info:
     unique_courses.add(course)
 print(unique_courses)
+
+# List students enrolled in English
+print("Students enrolled in English: ", end=" ")
+for student, course in info:
+    if course == "English":
+        print("[" + student + "]", end=" ")
+print()
+
+# Create a dictionary (student, set of courses)
+
+enrollments = {}
+
+for student, course in info:
+    if student not in enrollments:
+        enrollments[student] = set()
+    enrollments[student].add(course)
+
+print("Enrollments:", enrollments)
