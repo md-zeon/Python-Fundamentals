f0d28da88b443bdf05958e0059859611065ccb2b
md-zeon <zeon.cse@gmail.com>
2026-03-10 11:25:15 +0600

    Add assignment questions for string manipulation, lists, tuples, and dictionaries

---
+++ part-3/09.Assignment.py
@@ -0,0 +1,70 @@
+'''
+Q1. Ask the user for a string and check whether it is a palindrome or not. 
+A palindrome is a string which is same when we read it forward & backward. Eg - 
+“madam”, “racecar” etc.
+
+[ Hint- A palindrome string is equal to the reversed version of the string. We can 
+use a loop to reverse the string manually. ]
+'''
+
+
+'''
+Q2. Given a list of integers compute the average of all numbers in the list.
+'''
+
+'''
+Q3. Input two lists of integers from the user. Merge them into one list and sort the 
+result.
+Eg -  list1 = [1, 2, 7],  
+list2 = [2, 4, 5]
+result = [1, 2, 3, 54, 5, 7]
+'''
+
+'''
+Q4. Given a tuple of integers, create:
+• A tuple of all even numbers
+• A tuple of all odd numbers
+'''
+
+
+'''
+Q5. Create a dictionary where:
+• Keys = student names
+• Values = marks (integer)
+Write a menu-based program where user presses a key (ʼAʼ, ‘Bʼ, ‘Cʼ, ‘Dʼ) 
+depending on the operation they want to perform on the dictionary:
+1. A - Add a student
+2. B - Update marks
+3. C - Search for a student
+4. D - Display all students and marks
+'''
+
+
+'''
+Q6. Given a list of words:
+words = ["apple", "banana", "kiwi", "cherry", "mango"]
+Create a dictionary that maps each word to its length.
+Example:
+{"apple": 5, "banana": 6, "kiwi": 4, ...}
+'''
+
+'''
+Q7. Write a program that takes a string from the user and prints the number of 
+spaces in the string.
+'''
+
+'''
+Q8. Write a program to check whether two lists share no common elements. 
+# share no common elements list1 = [1, 2, 3, 4] list2 = [5, 6, 7, 8]
+# share common elements list1 = [1, 2, 3] list2 = [3, 4]
+'''
+
+'''
+Q9. Given a list, print all elements that appear more than once in the list.
+'''
+
+'''
+Q10. Ask the user for a string and print:
+• All unique characters
+• The count of unique characters
+'''
