5aead14536a476c4ee0d35c33669ece703ec513f
md-zeon <zeon.cse@gmail.com>
2026-03-13 22:22:46 +0600

    Add documentation for Object-Oriented Programming principles and pillars

---
+++ part-4/09.OOP_Pillars.py
@@ -0,0 +1,11 @@
+'''
+Object-Oriented Programming (OOP) is a programming paradigm that uses "objects" to design applications and computer programs. It is based on the concept of "classes" and "objects". The four main pillars of OOP are:
+1. Encapsulation
+2. Abstraction
+3. Inheritance
+4. Polymorphism
+1. Encapsulation: This is the concept of bundling data and methods that operate on that data within a single unit, or class. It also restricts direct access to some of an object's components, which can prevent the accidental modification of data.
+2. Abstraction: This is the concept of hiding the complex implementation details and showing only the necessary features of an object. It allows the programmer to focus on interactions at a higher level without needing to understand the underlying complexity.
+3. Inheritance: This is the mechanism by which one class can inherit properties and behaviors (fields and methods) from another class. It promotes code reusability and establishes a natural hierarchical relationship between classes.
+4. Polymorphism: This is the ability of different classes to be treated as instances of the same class through a common interface. It allows for methods to be used interchangeably, even if they belong to different classes, as long as they share the same interface or parent class.
+'''
