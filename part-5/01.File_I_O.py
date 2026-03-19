aec012a1aca679ba58491794e9d20db4a0dc7013
md-zeon <zeon.cse@gmail.com>
2026-03-19 19:19:08 +0600

    Added File I/O documentation: explained reading and writing operations in Python

---
+++ part-5/01.File_I_O.py
@@ -0,0 +1,9 @@
+'''
+File I/O (Input/Output) in Python allows you to read from and write to files. This is essential for data storage, manipulation, and retrieval. The built-in `open()` function is used to open a file, and it returns a file object that can be used to read or write data.
+some common file I/O operations:
+- `read()` - reads the entire file
+- `readline()` - reads a single line from the file
+- `readlines()` - reads all lines from the file and returns a list
+- `write()` - writes data to the file
+- `writelines()` - writes a list of strings to the file
+'''
